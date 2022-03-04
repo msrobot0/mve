595719c4e4a146021c024c251caf6a8d30b8ed01
drew <drew@drews-MBP.attlocal.net>
2022-03-04 08:53:55 -0600

    3m - and some basic syntax fixes, still issues with openzeppeli version of sol

--- truffle-config.js
+++ truffle-config.js
@@ -10,20 +10,20 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*"
-    }
+    },
     main: {
       provider: () => new HDWallerProvide({privateKeys:["PRIVE"],
-      providerOrUrl: "infura url
+      providerUrl: "infura url"
       }),
       network_id:1,
       gas: 3000000,
       gasPrice: 10000000000000,
-  },
-  compilers: {
-    solc: {
-      version: "0.8.3"
+    },
+    compilers: {
+      solc: {
+        version: "0.8.3"
+      }
     }
-  
   }
   //
   //
