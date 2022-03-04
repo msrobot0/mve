7897d5170bcca6b71592d998e356c1d05978a1e0
meredith <meredith@paom.com>
2022-03-04 08:52:15 -0500

    fixed sol version issues in Migrations.sol

--- contracts/MVE.sol
+++ contracts/MVE.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.3;
 
-import "hardhat/console.sol";
+//import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
