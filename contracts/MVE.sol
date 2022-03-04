b3f6c51ae97a8d3a6a62b851d71fbd4ed59f5b58
meredith <meredith@paom.com>
2022-03-04 08:45:08 -0500

    version issues

---
+++ contracts/MVE.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract MVE is ERC20 {
+
+    constructor() ERC20("MinimumViable", "MVE") public {
+        _mint(msg.sender, 1000000 * 10 ** 18);
+    }
+
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+const Poing = artifacts.require("Poing");
+
+module.exports = function(deployer) {
+  deployer.deploy(Poing);
+};
